0e3de0aca467884c65d17c7c25760937fdc29a2b
Harsh Soni <algebra.soni@gmail.com>
2024-02-06 16:36:16 +0000

    print, declare mut var, input

--- learn_rust/src/main.rs
+++ learn_rust/src/main.rs
@@ -5,5 +5,15 @@
 use std::io;
 
 fn main() {
-    println!("Hello, world!");
+    // anything with a "!"" is a Macro
+    println!("What's your name?");
+    
+    // mutable variable name, by default all the are variables in rust are immutable
+    let mut name = String::new();
+
+    // take input from user + &mut is reference to variable that allows to save value directly to name
+    // read_line returns enumerated type result
+    io::stdin().read_line(&mut name).expect("Naam toh batao apna!");
+
+    println!("Hello {}", name.trim_end());  // trim_end() gets rid of new line
 }
